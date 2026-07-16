9f0756869c5c868143862bdcd46deaae2626cd0c
Federico Pérez Errisuriz <federico.pereze.pe@gmail.com>
2026-07-15 22:07:48 -0600

    feat: support to firefox and safari

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
 import "@/css-custom/index.css";
+import { applyGlassSupportAttribute } from "./components-custom/GlassSuport";
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { LiquidGlassFilter } from '@/components-custom/liquid-glass-filter'
 import App from "./App";
 import { TooltipProvider } from "@/components/ui/tooltip"
 import { Toaster } from "@/components/ui/sonner"
+
+applyGlassSupportAttribute();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
