10f6fbea76b4c23a83263b6b7d06da7ceeab4ff0
Federico Pérez Errisuriz <federico.pereze.pe@gmail.com>
2026-07-31 00:24:44 -0600

    feat: to the new URL

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -12,10 +12,10 @@ function App() {
   const [selectedDay, setSelectedDay] = useState("dia1");
   const [total, setTotal] = useState(0);
 
-  //https://sistemadeinscripciones.onrender.com/
+  //https://mi-api-latest-4szb.onrender.com/
 
   useEffect(() => {
-    fetch('https://sistemadeinscripciones.onrender.com/api/registro/count', {})
+    fetch('https://mi-api-latest-4szb.onrender.com/api/registro/count', {})
     .then((res) => res.json())
     .then((data) => setTotal(data.total))
     .catch((err) => console.error(err));
